34de20750d4066fa1a683b7fde3bc8c833c0472a
kamal <ichar.soufiane@adias.fr>
2019-08-03 14:38:55 +0200

    commit projet

---
+++ src/main/java/com/cocacola/MyCompanyWithTestApplication.java
@@ -0,0 +1,13 @@
+package com.cocacola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCompanyWithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCompanyWithTestApplication.class, args);
+	}
+
+}
